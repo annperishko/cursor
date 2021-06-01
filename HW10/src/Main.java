83aff23da64095333a36596f743aecdd98fa5465
annperishko <perishko32@gmail.com>
2021-06-01 11:52:16 +0300

    HW10

---
+++ HW10/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[1000000000];
+        for (int i = 0; ; ) {
+            arr[i] = i;
+        }
+
+    }
+}
